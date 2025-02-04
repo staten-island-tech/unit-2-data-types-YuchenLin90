9766ce5e65b0ec9522072f92177cc9835c714347
YuchenLin90 <YUchenl27@nycstudents.net>
2025-02-04 10:02:10 -0500

    commit

---
+++ app.py
@@ -0,0 +1,32 @@
+# # #Strings are for representing characters, names, words, etc...
+# # name = "David"
+# # #Intergers represent whole numbers
+# # age: 14
+# # #Floats represent decimals
+# # wallet  = 5.45
+# # #Boolean represents true or false, used in evaluations
+# # graduated = False
+
+# # def add(x,y):
+# #     print(x + y)
+#     #input asks the user a question and stores their response as a value
+# bill = float(input("What was the bill"))
+# # #print(type(bill))
+# # add(40, bill)
+
+
+# #lists
+# # students = ["Joanna, "David", "Deivid", "Other David", "Ethan"]
+# #             #similar to saying for i in range(5) : print(students[i]
+# # for student in students:
+# #     print(student) 
+
+# moneys = [1,2,3,4,5,6]
+# total = 0
+# for money in moneys:
+#     total = total + money
+#     print(total)
+
+x = 3
+y = float(3)
+print(x,y)
