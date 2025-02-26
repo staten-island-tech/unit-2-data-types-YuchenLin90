58cd920ef88530eae1fd84b9ec876134a706365f
YuchenLin90 <YUchenl27@nycstudents.net>
2025-02-26 10:14:43 -0500

    commit

---
+++ assessment.py
@@ -0,0 +1,49 @@
+# x = input("Type here:")
+# def word_count(x):
+#     y = x.split()
+#     return len(y)
+# y = word_count
+
+
+# number_t = "t"
+# number_T = "T"
+# number_s = "s"
+# number_S = "S"
+
+
+# Y = input([])
+# x = (f"Sentences{[Y]}")
+# def letters(t, T, s, S):
+#     for i in range(x):
+#         if t and T > 5 or t and T == s and S:
+#             print("English")
+#         else:
+#             print("French")
+
+
+
+
+
+
+
+# if number_t or number_T > 5:
+#     print("English")
+# else:
+#     print("French")
+
+
+
+x = input("words:")
+def letter(T, t, S, s):
+    letter = []
+    letter = x
+    for i in letter():
+        x.split
+        if T and t:
+            letter.append(i)
+        if T and t > 5 or S and s == T and t:
+            print ("English")
+            
+        else:
+            print("French")
+
